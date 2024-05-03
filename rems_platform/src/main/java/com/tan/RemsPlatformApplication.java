a578ded034034d07da1bf962fc354d2acb55737b
always <tan@akk.com>
2024-05-03 13:23:20 +0800

    搭建

---
+++ rems_platform/src/main/java/com/tan/RemsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemsPlatformApplication.class, args);
+    }
+
+}
---
+++ rems_platform/src/main/java/com/tan/controller/ControllerPatient.java
@@ -0,0 +1,11 @@
+package com.tan.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by TanLiangJie
+ * Time:2024/5/3 下午1:22
+ */
+@RestController
+public class ControllerPatient {
+}
